9a589bdcc574d7ed0cb688bd34660af26b9fd8e5
pallasite99 <salil.apte99@gmail.com>
2022-06-24 15:37:26 +0530

    establish connection to mysql server

---
+++ config.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2')
+
+const config = mysql.createConnection({
+
+  // done for testing, do not expose sensitive data like this anywhere
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'customer_schema'
+})
+
+config.connect(function (err) {
+  if (err) {
+    return console.error('error: ' + err.message)
+  }
+
+  console.log('Connected to the MySQL server.')
+})
