93c20b07e616019d160bf469e4c0db1f98fc018f
Salil Apte <salil.apte99@gmail.com>
2022-06-24 13:56:46 +0530

    Update index.js

--- pkg/api/v1/index.js
+++ pkg/api/v1/index.js
@@ -1,1 +1,17 @@
 use strict;
+
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
