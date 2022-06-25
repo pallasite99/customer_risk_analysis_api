9055fe26b036980130b8b3795f2e78c44a53fed4
pallasite99 <salil.apte99@gmail.com>
2022-06-25 06:36:56 +0530

    initial API to fetch identities of risky customers

--- pkg/api/v1/index.js
+++ pkg/api/v1/index.js
@@ -1,4 +1,12 @@
 const express = require('express')
+const config = require('../../../config')
+const mysql = require('mysql2')
+const pool = mysql.createPool({
+  user: 'root',
+  password: 'root',
+  database: 'customer_schema',
+  host: 'localhost'
+})
 const app = express()
 const port = 3000
 app.use(express.json())
@@ -13,3 +21,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get("/risky_identities/:customer_id", async(req, res) => {
+  const query = "SELECT email, phone, creditcard FROM test_view WHERE customer_id = ?";
+  pool.query(query, [ req.params.customer_id ], (error, results) => {
+    if(error) {
+      res.json({error: error.message});
+    }
+    if(!results[0]) {
+      res.json({status: '404 not found!'});
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
