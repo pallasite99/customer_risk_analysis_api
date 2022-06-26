401a1bc16a560fe3fdbc5e4d9edf684f0a0e9419
pallasite99 <salil.apte99@gmail.com>
2022-06-26 20:32:15 +0530

    add insert API for db invoke

--- pkg/api/v1/insert.js
+++ pkg/api/v1/insert.js
@@ -33,11 +33,39 @@ app.listen(port, () => {
   console.log(`insert app listening at http://localhost:${port}`)
 })
 
-// insert given custpmer_id and email into risky_email table
+// insert given customer_id and email into risky_email table
 // returns status 200 if OK else error status code
 app.get('/insert/risky_email/:customer_id/email/:email', (req, res) => {
   const insertEmailQuery = 'INSERT INTO risky_email VALUES (?, ?)'
   pool.query(insertEmailQuery, [req.params.customer_id, req.params.email], (error, results) => {
+    if (error) {
+      res.json(error)
+    } else {
+      res.sendStatus(200)
+      res.json(results[0])
+    }
+  })
+})
+
+// insert given customer_id and phone into risky_phone table
+// returns status 200 if OK else error status code
+app.get('/insert/risky_phone/:customer_id/phone/:phone', (req, res) => {
+  const insertEmailQuery = 'INSERT INTO risky_phone VALUES (?, ?)'
+  pool.query(insertEmailQuery, [req.params.customer_id, req.params.phone], (error, results) => {
+    if (error) {
+      res.json(error)
+    } else {
+      res.sendStatus(200)
+      res.json(results[0])
+    }
+  })
+})
+
+// insert given customer_id and creditcard into risky_creditcard table
+// returns status 200 if OK else error status code
+app.get('/insert/risky_creditcard/:customer_id/creditcard/:creditcard', (req, res) => {
+  const insertEmailQuery = 'INSERT INTO risky_creditcard VALUES (?, ?)'
+  pool.query(insertEmailQuery, [req.params.customer_id, req.params.creditcard], (error, results) => {
     if (error) {
       res.json(error)
     } else {
